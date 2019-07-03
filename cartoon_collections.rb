f3d4f3c1b2eb47022e52c841b596f73a20b7de3f
fortuitous-counter-8979 <gmcharmy@gmail.com>
2019-07-03 02:05:58 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,5 @@
 def roll_call_dwarves(dwarves)
- dwarves.map.with_index{|el, idx| puts "#{idx}.*#{el}"}
+ dwarves.each_with_index{|el, idx| puts "#{idx+1}.*#{el}"}
 end
 
 def summon_captain_planet# code an argument here
