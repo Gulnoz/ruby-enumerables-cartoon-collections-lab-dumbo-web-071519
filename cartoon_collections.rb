75ac71f614906350037ebfef0f7672ccfe0bebb8
fortuitous-counter-8979 <gmcharmy@gmail.com>
2019-07-03 02:29:49 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,7 +15,8 @@ def long_planeteer_calls(calls)
  false
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(if_cheese)
+  
   cheese_types = ["cheddar", "gouda", "camembert"]
+  if_cheese
 end
