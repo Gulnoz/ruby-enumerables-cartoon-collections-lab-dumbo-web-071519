2d12c9c08f20c9bb76c300fb28bf0b74a3a7c401
fortuitous-counter-8979 <gmcharmy@gmail.com>
2019-07-03 02:25:52 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,7 +7,7 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(calls)
-  return calls.each{|call| call.length>4 ? true:false}
+  calls.each{|call| call.length>4 ? return true:false}
  
 end
 
